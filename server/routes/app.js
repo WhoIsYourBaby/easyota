70beb0f1ae85474c04505175586f202634a0b911
ligun <ligun@cechealth.cn>
2020-12-08 18:27:49 +0800

    修正路径

--- server/routes/app.js
+++ server/routes/app.js
@@ -3,7 +3,7 @@ const dbhealper = require('../utils/dbhealper');
 const multer = require('@koa/multer');
 const path = require('path');
 const upload = multer({
-  dest: path.join(__dirname, '/public/upload'),
+  dest: path.join(__dirname, '../public/upload'),
 });
 
 router.prefix('/app');
