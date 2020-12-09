245cd0340b29d568d51d93b90904369074d17a02
ligun <ligun@cechealth.cn>
2020-12-09 16:49:53 +0800

    解析ipa+apk

--- server/routes/app.js
+++ server/routes/app.js
@@ -2,6 +2,9 @@ const router = require('koa-router')();
 const dbhealper = require('../utils/dbhealper');
 const multer = require('@koa/multer');
 const path = require('path');
+const UUID = require('node-uuid');
+const AppInfoParser = require('app-info-parser');
+
 const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
@@ -9,7 +12,13 @@ const upload = multer({
     },
     //修改文件名称
     filename: (req, file, cb) => {
-      cb(null, file.originalname);
+      let filename = UUID.v1();
+      filename = filename.replace(/-/g, '');
+      if (file.originalname.indexOf('.') > 0) {
+        let suffix = file.originalname.split('.').pop();
+        filename = filename + '.' + suffix;
+      }
+      cb(null, filename);
     }
   })
 });
@@ -34,10 +43,48 @@ router.get('/list', async (ctx, next) => {
 
 router.post('/upload', upload.single('file'), async (ctx, next) => {
   console.log('ctx.file', ctx.file);
+  const match = ctx.file.filename.match('(.apk$)|(.ipa$)');
+  let platform = -1;
+  if (ctx.file.filename.match('(.apk$)')) {
+    platform = 0; //android
+  }
+  if (ctx.file.filename.match('(.ipa$)')) {
+    platform = 1;
+  }
+  if (platform === -1) {
+    ctx.body = {
+      code: 500,
+      msg: '请上传ipa或者apk安装包'
+    };
+    return;
+  }
+  const path = ctx.file.path;
+  const parser = new AppInfoParser(path);
+  const appinfo = await parser.parse();
+  let handleResult;
+  if (platform === 0) {
+    handleResult = await handleAndroid(ctx.state.sqlconn, ctx.state.user, appinfo);
+  } else {
+    handleResult = await handleIos(ctx.state.sqlconn, ctx.state.user, appinfo);
+  }
   ctx.body = {
     code: 200,
-    msg: 'ok'
+    msg: 'ok',
+    body: handleResult
   };
 });
+
+async function handleIos(conn, user, appinfo) {
+  //0 判断是否已经存在
+  const bundleId = appinfo.CFBundleIdentifier;
+  const version = appinfo.CFBundleShortVersionString;
+  const build = appinfo.CFBundleVersion;
+  //1 如果已经存在则升级
+  //2 如果不存在则新生成app+版本
+  const insert = '';
+  const apps = await dbhealper.makePromise(ctx.state.sqlconn, query, [user.id]);
+}
+
+async function handleAndroid(conn, user, appinfo) {}
 
 module.exports = router;
