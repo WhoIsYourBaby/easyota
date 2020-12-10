0045b54de95e775dec5d26792616262736c2920a
ligun <ligun@cechealth.cn>
2020-12-10 20:52:40 +0800

    请求中添加token

--- web/src/views/login/index.vue
+++ web/src/views/login/index.vue
@@ -9,7 +9,7 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">easyota</h3>
+        <h3 class="title">EASYOTA</h3>
       </div>
 
       <el-form-item prop="username">
