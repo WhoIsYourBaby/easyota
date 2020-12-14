942bed4816c0ec6df60e195a9680a18059262b45
ligun <ligun@cechealth.cn>
2020-12-14 15:05:26 +0800

    修正token后去问题

--- web/src/store/getters.js
+++ web/src/store/getters.js
@@ -1,8 +1,5 @@
-import { getToken } from "@/utils/auth"
-
 const getters = {
   sidebar: state => state.settings.sidebar,
   device: state => state.settings.device,
-  token: state => getToken(),
 }
 export default getters
