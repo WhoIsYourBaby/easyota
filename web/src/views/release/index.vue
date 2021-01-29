6605c891e5ba3aff5935fbef32cbf06699626f37
ligun <ligun@cechealth.cn>
2021-01-29 10:30:16 +0800

    加入mobile判断

--- web/src/views/release/index.vue
+++ web/src/views/release/index.vue
@@ -2,12 +2,20 @@
   <div>
     <div>{{ $route.params.short }}</div>
     <div>{{ $route.params.verId }}</div>
+    <div>{{ isMobile ? 'mobile' : 'desktop' }}</div>
   </div>
 </template>
 
 <script>
 // 同时支持mobile、desktop
+import ResizeMixin from '@/layout/mixin/ResizeHandler';
 export default {
+  mixins: [ResizeMixin],
+  computed: {
+    isMobile() {
+      return this.$store.state.settings.device == 'mobile';
+    }
+  },
   mounted() {
     console.log(this.$route.params);
   }
