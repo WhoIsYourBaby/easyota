517cdd2390ef84acb572c55ef44c1a483ec5a0e9
ligun <ligun@cechealth.cn>
2021-01-31 18:24:23 +0800

    桌面下载页

--- web/src/views/release/desktop/Default.vue
+++ web/src/views/release/desktop/Default.vue
@@ -1,3 +1,85 @@
 <template>
-  <div>我是default desktop</div>
+  <div class="mydefault">
+    <el-image
+      style="width: 120px; height: 120px; margin-top: 120px"
+      :src="data.icon"
+      fit="fill"
+    ></el-image>
+    <main-title style="margin-top: 20px">{{ this.data.name }}</main-title>
+    <text-label style="margin-top: 20px">
+      版本：{{ this.data.version.version }} (build {{ this.data.version.build }})
+    </text-label>
+    <text-label style="margin-top: 10px">大小：39.1 MB</text-label>
+    <text-label style="margin-top: 10px">
+      更新时间：{{ formatDate(this.data.version.createTime) }}
+    </text-label>
+    <vue-qr
+      style="margin-top: 20px"
+      class="bicode"
+      :text="qrUrl"
+      :size="256"
+      :logoSrc="data.icon"
+      :margin="8"
+    ></vue-qr>
+    <div style="width: 70%">
+      <el-divider></el-divider>
+      <main-title>更新说明</main-title>
+      <text-body style="margin-top: 20px">{{ this.data.version.vdesc }}</text-body>
+    </div>
+    <div style="width: 70%">
+      <el-divider></el-divider>
+      <main-title>应用介绍</main-title>
+      <text-body style="margin-top: 20px">{{ this.data.adesc }}</text-body>
+    </div>
+  </div>
 </template>
+
+<script>
+import VueQr from 'vue-qr';
+import {formatDate} from '@/utils/validate';
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: null
+    }
+  },
+  components: {
+    VueQr
+  },
+  mounted() {},
+  computed: {
+    qrUrl() {
+      let pageurl = this.data.shortUrl;
+      if (this.$route.query.verUuid) {
+        pageurl = pageurl + `?verUuid=${this.$route.query.verUuid}`;
+        if (this.$route.query.branch) {
+          pageurl = pageurl + `&branch=${this.$route.query.branch}`;
+        }
+      } else if (this.$route.query.branch) {
+        pageurl = pageurl + `?branch=${this.$route.query.branch}`;
+      }
+      return pageurl;
+    }
+  },
+  methods: {
+    formatDate(value) {
+      return formatDate('yyyy-MM-dd hh:mm', value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.mydefault {
+  @include flexStart;
+  align-items: center;
+  flex-direction: column;
+
+  .textpart {
+    @include flexStart;
+    align-items: flex-start;
+    flex-direction: column;
+  }
+}
+</style>
--- web/src/views/release/mobile/Default.vue
+++ web/src/views/release/mobile/Default.vue
@@ -1,3 +1,40 @@
 <template>
-  <div>我是default mobile</div>
+  <div class="mydefault">
+    <el-image
+      style="width: 120px; height: 120px; margin-top: 120px"
+      :src="data.icon"
+      fit="fill"
+    ></el-image>
+    <main-title style="margin-top: 20px">{{ this.data.name }}</main-title>
+    <text-label style="margin-top: 20px">
+      版本：{{ this.data.version.version }} (build {{ this.data.version.build }})
+    </text-label>
+    <text-label style="margin-top: 10px">大小：39.1 MB</text-label>
+    <text-label style="margin-top: 10px">更新时间：{{ formatDate(this.data.version.createTime) }}</text-label>
+  </div>
 </template>
+
+<script>
+import {formatDate} from '@/utils/validate';
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    formatDate(value) {
+      return formatDate('yyyy-MM-dd hh:mm', value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.mydefault {
+  @include flexStart;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
