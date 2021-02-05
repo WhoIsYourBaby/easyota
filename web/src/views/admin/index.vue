f6de2d66bc82e5cf409e0ca97892de683089dcc8
ligun <ligun@cechealth.cn>
2021-02-05 18:08:48 +0800

    添加oss输入ui

--- web/src/views/admin/index.vue
+++ web/src/views/admin/index.vue
@@ -2,11 +2,8 @@
   <div class="admin-container">
     <div v-if="user.type == 'admin'">
       <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="活动名称">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
         <el-form-item label="允许注册">
-          <el-switch v-model="form.delivery"></el-switch>
+          <el-switch v-model="form.allowRegister"></el-switch>
         </el-form-item>
         <el-form-item label="存储方式">
           <el-radio-group v-model="form.store">
@@ -16,6 +13,20 @@
             <el-radio label="tecent">腾讯云COS</el-radio>
           </el-radio-group>
         </el-form-item>
+        <div>
+          <el-form-item label="key">
+            <el-input v-model="form.oss.key" placeholder="Access Key"></el-input>
+          </el-form-item>
+          <el-form-item label="secret">
+            <el-input v-model="form.oss.secret" placeholder="Secret Key"></el-input>
+          </el-form-item>
+          <el-form-item label="endpoint">
+            <el-input v-model="form.oss.endpoint"></el-input>
+          </el-form-item>
+          <el-form-item label="bucket">
+            <el-input v-model="form.oss.bucket"></el-input>
+          </el-form-item>
+        </div>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">立即保存</el-button>
         </el-form-item>
@@ -37,14 +48,14 @@ export default {
   data() {
     return {
       form: {
-        name: '',
         store: 'local',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        allowRegister: false,
+        oss: {
+          key: '',
+          secret: '',
+          endpoint: '',
+          bucket: '',
+        },
       }
     };
   },
