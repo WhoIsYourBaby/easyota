bee03bb1bc5f6b9c6ace4d997d77aeddf8643fcd
ligun <ligun@cechealth.cn>
2021-02-08 15:24:53 +0800

    todo：上传图片头像

--- web/src/views/users/index.vue
+++ web/src/views/users/index.vue
@@ -17,7 +17,7 @@
         </el-table-column>
       </el-table>
       <div class="admin-bottom">
-        <el-button type="primary" size="small">创建用户</el-button>
+        <el-button type="primary" size="small" @click="onAddUser">创建用户</el-button>
         <el-pagination
           background
           layout="prev, pager, next"
@@ -27,6 +27,32 @@
           @current-change="onPageChanged"
         ></el-pagination>
       </div>
+      <el-dialog title="新建用户" :visible.sync="isAddingUser">
+        <el-form :model="userAdded">
+          <el-form-item label="登录邮箱">
+            <el-input v-model="userAdded.email"></el-input>
+          </el-form-item>
+          <el-form-item label="昵称">
+            <el-input v-model="userAdded.nickname"></el-input>
+          </el-form-item>
+          <el-form-item label="上传头像">
+            <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="onAvatarSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="userAdded.avatar" :src="userAdded.avatar" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="isAddingUser = false">取 消</el-button>
+          <el-button type="primary" @click="onAddUserSubmit">确 定</el-button>
+        </div>
+      </el-dialog>
     </div>
     <div v-else>i am normal</div>
   </div>
@@ -47,13 +73,40 @@ export default {
       userList: [],
       page: 1,
       size: 10,
-      total: 0
+      total: 0,
+      isAddingUser: false,
+      userAdded: {
+        email: null,
+        nickname: null,
+        avatar: null
+      }
     };
   },
   mounted() {
     this.fetchUserList(1);
   },
   methods: {
+    beforeAvatarUpload(file) {
+      const isAllowType = file.type === 'image/jpeg' || file.type === 'image/png';
+      const isLt10M = file.size / 1024 / 1024 < 10;
+      if (!isAllowType) {
+        this.$message.error('上传头像图片只能是 JPG 或者 PNG 格式!');
+      }
+      if (!isLt10M) {
+        this.$message.error('上传头像图片大小不能超过 10MB!');
+      }
+      return isAllowType && isLt10M;
+    },
+    onAvatarSuccess(res, file) {
+      console.log(file);
+    },
+    //创建用户提交按钮
+    onAddUserSubmit() {
+      this.isAddingUser = false;
+    },
+    onAddUser() {
+      this.isAddingUser = true;
+    },
     onUserDelete(user) {
       this.$confirm(`确定要删除${user.email}这个用户吗？`, '提示', {
         confirmButtonText: '确定',
@@ -131,5 +184,28 @@ export default {
     @include flexBetween;
     flex-direction: row;
   }
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
